dbb0c769597a31fb0d174be60cdfc844b6e20c3f
Tata Sulysta <tata_ss@softwareseni.com>
2022-09-19 11:03:22 +0700

    add random generator function

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
 import styles from '../styles/Home.module.css';
+
+import randomGenerator from '../utils/helpers/randomGenerator';
 
 // export const getStaticProps = async () => {};
 
 export default function Home() {
+	const [number, setNumber] = useState();
 	useEffect(() => {
 		if (localStorage.getItem('POKEMON') !== {}) {
 			fetch('https://pokeapi.co/api/v2/pokemon?limit=100')
@@ -14,6 +17,15 @@ export default function Home() {
 					localStorage.setItem('POKEMON', JSON.stringify(data.results))
 				);
 		}
+		setNumber(randomGenerator());
 	}, []);
-	return <div className={styles.container}>test</div>;
+	const handleClick = () => {
+		setNumber(randomGenerator());
+	};
+	return (
+		<div className={styles.container}>
+			{number}
+			<button onClick={handleClick}>hello</button>
+		</div>
+	);
 }
---
+++ utils/helpers/randomGenerator.js
@@ -0,0 +1,4 @@
+export default function randomGenerator() {
+	const number = Math.trunc(Math.random() * 100);
+	return number;
+}
